7be63ecfc1122014c81e7c468e7b0085e0e3acb2
Kewyn Akshlley <kewynakshlley@gmail.com>
2024-02-21 16:41:01 -0300

    feat: Remove utils

--- thermal_parser/src/lib.rs
+++ thermal_parser/src/lib.rs
@@ -7,7 +7,6 @@ pub mod graphics;
 pub mod parser;
 pub mod subcommands;
 pub mod util;
-pub mod utils;
 
 pub fn new_esc_pos_parser(on_command_found: Box<dyn FnMut(command::Command)>) -> parser::Parser {
     parser::Parser::new(command_sets::esc_pos::new(), on_command_found)
