f559b1931a535483a52e7d1f4874f59c6dc3ed77
zachzurn <zachzurn@gmail.com>
2024-11-08 11:16:57 -0800

    Add debugging for set character size command

--- thermal_parser/src/commands/set_character_size.rs
+++ thermal_parser/src/commands/set_character_size.rs
@@ -7,17 +7,30 @@ struct Handler;
 impl CommandHandler for Handler {
     fn apply_context(&self, command: &Command, context: &mut Context) {
         let n = *command.data.get(0).unwrap_or(&0u8);
-
-        //last 4,5,6 bits masked
-        //   ---
-        //11110101 -> 00000101
-        context.text.height_mult = 0b00000111 & n + 1;
-
-        //bit 1,2,3 masked and shifted all the way to the right // ***
-        // ---
-        //01010101 -> 01010000 -> 00000101
-        context.text.width_mult = ((0b01110000 & n) >> 4) + 1;
+        let stretch = parse_stretch(n);
+        context.text.width_mult = stretch.0;
+        context.text.height_mult = stretch.1;
     }
+
+    fn debug(&self, command: &Command, _context: &Context) -> String {
+        let n = *command.data.get(0).unwrap_or(&0u8);
+        let stretch = parse_stretch(n);
+        format!("{} Stretch w{} h{}", command.name, stretch.0, stretch.1)
+    }
+}
+
+fn parse_stretch(value: u8) -> (u8, u8) {
+    //last 4,5,6 bits masked
+    //   ---
+    //11110101 -> 00000101
+    let h = 0b00000111 & value + 1;
+
+    //bit 1,2,3 masked and shifted all the way to the right // ***
+    // ---
+    //01010101 -> 01010000 -> 00000101
+    let w = ((0b01110000 & value) >> 4) + 1;
+
+    (w,h)
 }
 
 pub fn new() -> Command {
